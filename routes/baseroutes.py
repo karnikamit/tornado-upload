ac49f6e52289a53bf8065e4148b384a26b1c6215
karnikamit <amit.karnik@edgenetworks.in>
2015-07-27 15:27:48 +0530

    Still under building stage

--- routes/baseroutes.py
+++ routes/baseroutes.py
@@ -3,11 +3,13 @@ from flask import jsonify, request, render_template
 from routes import app
 from Downloads.download import Download
 
+
 @app.route('/try', methods=["GET"])
-def app_try():
+def api_try():
     return jsonify({"response": "success"})
 
-@app.route('/Download', methods=["POST", "GET"])
+
+@app.route('/get_file', methods=["POST", "GET"])
 def download_file():
     path = request.json
     do = Download(path)
@@ -16,3 +18,13 @@ def download_file():
         return jsonify({"response": "success"})
     except Exception, e:
         return jsonify({"response": "failure", "msg": str(e)})
+
+
+@app.route("/Download", methods=["GET"])
+def download_api():
+    return render_template('download_form.html')
+
+
+@app.route("/Upload", methods=["GET"])
+def upload_api():
+    return render_template('upload_form.html')
