d5272f8ca2338d4f24a498f1e3b269aa4cf26034
karnikamit <amit.karnik@edgenetworks.in>
2015-07-27 15:49:55 +0530

    Still under building stage

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,6 +1,7 @@
 __author__ = 'karnikamit'
 from flask import Flask
-
-app = Flask(__name__)
+import os
+app_route = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
+app = Flask(__name__, template_folder=app_route+'/templates')
 from routes import baseroutes
 
