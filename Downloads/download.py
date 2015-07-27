93c64e7731e6a1552d4f0e33b345b1c6ec4bec2f
karnikamit <amit.karnik@edgenetworks.in>
2015-07-27 15:20:26 +0530

    Still under building stage

--- Downloads/download.py
+++ Downloads/download.py
@@ -1,15 +1,10 @@
 __author__ = 'karnikamit'
-from simpleconfigparser import simpleconfigparser
-import os
 import StringIO
 from flask import send_file
-configfile = simpleconfigparser()
 
 
 class Download:
     def __init__(self, path):
-        app_route = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
-        configfile.read(app_route+'/Downloads/down.ini')
         self.path = path
         self.f_name = (self.path.split('.')[0]).split('/')[-1]
         self.extn = self.path.split('.')[-1]
